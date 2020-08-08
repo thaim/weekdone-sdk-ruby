90d1ed7294afc43315ec6ec92c4f8c08440219f0
thaim <thaim24@gmail.com>
2020-08-08 17:13:18 +0000

    define all APIs

--- lib/weekdone/api.rb
+++ lib/weekdone/api.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require 'oauth2'
+require 'faraday'
 
 class Weekdone::Api
-  attr_reader :token_client, :token_code
+  API_URL = 'https://api.weekdone.com'
+  attr_reader :client, :token_client, :token_code
 
   def initialize(client_id, client_secret)
-    client = OAuth2::Client.new(
+    @client = OAuth2::Client.new(
       client_id, client_secret,
       {
         site: 'https://weekdone.com',
@@ -15,6 +17,9 @@ class Weekdone::Api
         redirect_uri: 'http://localhost:8080/oauth2/callback'
       }
     )
+  end
+
+  def authenticate
     puts "open URL to authorize:"
     puts client.auth_code.authorize_url
 
@@ -29,11 +34,153 @@ class Weekdone::Api
     @token_code = @token_client.token
   end
 
-  def listItems
-    response = token_client.get(
-      '/1/items'
-    )
+  def searchForItems(user_id: nil, team_id: nil, period: nil)
+    params = { token: @token_code }
+    if not user_id.nil?
+      params[:user_id] = user_id
+    end
+    if not team_id.nil?
+      params[:team_id] = team_id
+    end
+    if not period.nil?
+      params[:period] = period
+    end
 
-    response.body
+    response = Faraday.get(API_URL + '/1/items', params)
+    JSON.parse(response.body)
+  end
+
+  def createItem
+    raise NotImplementedError
+  end
+
+  def updateItem
+    raise NotImplementedError
+  end
+
+  def assignItemToAnotherUser
+    raise NotImplementedError
+  end
+
+  def deleteItem
+    raise NotImplementedError
+  end
+
+  def getItemLikes
+    raise NotImplementedError
+  end
+
+  def addItemLike
+    raise NotImplementedError
+  end
+
+  def deleteItemLike
+    raise NotImplementedError
+  end
+
+  def sortItems
+    raise NotImplementedError
+  end
+
+  def getItemComments
+    raise NotImplementedError
+  end
+
+  def addItemComment
+    raise NotImplementedError
+  end
+
+  def deleteItemComment
+    raise NotImplementedError
+  end
+
+
+  def getReport
+    params = { token: @token_code }
+
+    response = Faraday.get(API_URL + '/1/report', params)
+    JSON.parse(response.body)
+  end
+
+
+  def getAllTeams
+    raise NotImplementedError
+  end
+
+
+  def getAllUsers
+    raise NotImplementedError
+  end
+
+
+  def getAllTypes
+    raise NotImplementedError
+  end
+
+
+  def getAllTags
+    raise NotImplementedError
+  end
+
+  def getSingleTag
+    raise NotImplementedError
+  end
+
+  def updateTagPriority
+    raise NotImplementedError
+  end
+
+  def updateTagStatus
+    raise NotImplementedError
+  end
+
+
+  def getAllObjectives
+    raise NotImplementedError
+  end
+
+  def createNewObjective
+    raise NotImplementedError
+  end
+
+  def updateObjective
+    raise NotImplementedError
+  end
+
+  def deleteObjective
+    raise NotImplementedError
+  end
+
+  def listObjectiveComments
+    raise NotImplementedError
+  end
+
+  def addObjectiveComment
+    raise NotImplementedError
+  end
+
+  def updateObjectiveComment
+    raise NotImplementedError
+  end
+
+  def deleteObjectiveComment
+    raise NotImplementedError
+  end
+
+  def addObjectiveResult
+    raise NotImplementedError
+  end
+
+  def updateObjectiveResult
+    raise NotImplementedError
+  end
+
+  def deleteObjectiveResult
+    raise NotImplementedError
+  end
+
+
+  def getCompanyInfo
+    raise NotImplementedError
   end
 end
--- spec/weekdone_api_spec.rb
+++ spec/weekdone_api_spec.rb
@@ -5,7 +5,42 @@ RSpec.describe Weekdone do
 
     sut = Weekdone::Api.new(client_id, client_secret)
 
-    expect(sut.token_client).to_not be nil
-    expect(sut.token_code).to_not   be nil
+    expect(sut).not_to be nil
+    expect(sut.token_client).to be nil
+    expect(sut.token_code).to   be nil
+  end
+
+
+  describe '#authenticate' do
+  end
+
+
+  context 'with valid auth code' do
+    before(:all) do
+      client_id = ENV['WEEKDONE_CLIENT_ID']
+      client_secret = ENV['WEEKDONE_CLIENT_SECRET']
+
+      @sut = Weekdone::Api.new(client_id, client_secret)
+      @sut.authenticate
+    end
+
+    describe '#searchForItems' do
+      it 'search for items without search parameter' do
+        items = @sut.searchForItems
+
+        expect(items['status']).to eq('ok')
+        expect(items['items'].length).to be >= 0
+      end
+    end
+
+    describe '#getReport' do
+      it 'get report' do
+        report = @sut.getReport
+
+        expect(report['status']).to eq('ok')
+        expect(report['period']).not_to be nil
+        expect(report['report'].length).to be >= 0
+      end
+    end
   end
 end
